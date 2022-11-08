3f1a3a35c8bba16de73330a625a68f0050deb5b4
BarrioTropical <barriotropical@gmail.com>
2022-11-08 16:56:04 +0100

    js mail e print in html
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
+/*
+Attraverso l'apposita API di Boolean :faccia_che_festeggia:
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 "use strict"
 
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
+createApp({
+    data(){
+        return{
+            //array vuoto per le mail
+            mails: []
+        }
+    },
+    mounted() {
+        //ciclo for fino a 10
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                //push nell'array
+                this.mails.push(response.data.response)
+            })
+        }
     }
-  }).mount('#app')
+}).mount('#app')
